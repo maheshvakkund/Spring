79d676e9157902f817d326525cd615cdc7bf4a26
MAHESH RUDRAPPA VAKKUND <57062158+maheshvakkund@users.noreply.github.com>
2020-04-01 15:29:53 +0530

    Spring bean lifecycle : implemented customized init method and destroy using annotations

---
+++ spring-annotations-beanlifecycle/src/com/mahesh/springannotations/TennisCoach.java
@@ -0,0 +1,41 @@
+package com.mahesh.springannotations;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Mahesh
+ *
+ */
+@Component
+public class TennisCoach implements Coach {
+
+	@Autowired
+	FortuneService fortuneService;
+
+	@Override
+	public String getDailyWorkout() {
+		return "practice your back hand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("init-method : doMyStartupStuff");
+	}
+
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanStuff() {
+		System.out.println("destroy-method : doMyCleanStuff");
+	}
+
+}
